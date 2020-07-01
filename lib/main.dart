fe1c485b09a4839808d3817f5063ae4ce192fb46
peetacho <peterchow2001@gmail.com>
2020-07-02 02:27:35 +0800

    227am july2 page structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/app.dart';
 
 // void main() => runApp(App());
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.black, //top bar color
+    statusBarIconBrightness: Brightness.dark, //top bar icons
+    systemNavigationBarColor: Colors.black, //bottom bar color
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(new MaterialApp(
     home: new MyTextInput(),
   ));
@@ -26,6 +33,7 @@ class MyTextInputState extends State<MyTextInput> {
             style: TextStyle(color: Colors.black),
           ),
           elevation: 0.0,
+          brightness: Brightness.light,
           backgroundColor: Colors.white),
       body: new Container(
         child: new Center(
--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,6 +1,6 @@
 // app.dart
-import 'package:flutter/material.dart';
 import 'entry/entry_entry.dart';
+import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
@@ -17,38 +17,79 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  final Key keyOne = PageStorageKey('pageOne');
+  final Key keyTwo = PageStorageKey('pageTwo');
+  final Key keyThree = PageStorageKey('pageThree');
+  final Key keyFour = PageStorageKey('pageFour');
+
+  PageOne _pageOne;
+  PageTwo _pageTwo;
+  PageThree _pageThree;
+  PageFour _pageFour;
+  List<Widget> pages;
+  Widget currentPage;
+
+  final PageStorageBucket bucket = PageStorageBucket();
+
+  @override
+  void initState() {
+    _pageOne = PageOne(
+      key: keyOne,
+    );
+    _pageTwo = PageTwo(
+      key: keyTwo,
+    );
+    _pageThree = PageThree(
+      key: keyThree,
+    );
+    _pageFour = PageFour(
+      key: keyFour,
+    );
+
+    pages = [_pageOne, _pageTwo, _pageThree, _pageFour];
+    currentPage = _pageOne;
+    super.initState();
+  }
+
+  // COLORS //
   Color _indigo = Color.fromRGBO(98, 122, 239, 1);
   Color _indigo2 = Color.fromRGBO(149, 166, 244, 1);
-  Color _indigo_shadow = Color.fromRGBO(206, 214, 244, 0.6);
+  Color _indigoShadow = Color.fromRGBO(206, 214, 244, 0.6);
   Color _background = Color.fromRGBO(240, 243, 250, 1);
   Color _grey = Colors.grey[300];
 
-  Color _normal_icon_1 = Color.fromRGBO(149, 166, 244, 1);
-  Color _normal_icon_2 = Colors.grey[300];
-  Color _normal_icon_3 = Colors.grey[300];
-  Color _normal_icon_4 = Colors.grey[300];
+  // ICON COLORS //
+  Color _normalIcon1 = Color.fromRGBO(149, 166, 244, 1);
+  Color _normalIcon2 = Colors.grey[300];
+  Color _normalIcon3 = Colors.grey[300];
+  Color _normalIcon4 = Colors.grey[300];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-          title: new Text(
-            "PassVault",
-            style: TextStyle(color: Colors.black),
-          ),
-          elevation: 0.0,
-          backgroundColor: Colors.white),
+      appBar: PreferredSize(
+          child: AppBar(
+              title: new Text(
+                "PassVault",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              elevation: 0.0,
+              brightness: Brightness.light,
+              backgroundColor: Colors.white),
+          preferredSize: Size.fromHeight(60.0)),
       body: new Container(
         decoration: BoxDecoration(
           color: _background,
         ),
+        child: PageStorage(child: currentPage, bucket: bucket),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // runApp(Entry());
           debugPrint('add entry button pressed');
         },
-        elevation: 10,
+        elevation: 5,
         child: Container(
           decoration: BoxDecoration(
               border: Border.all(width: 9.0, color: _indigo),
@@ -56,7 +97,7 @@ class HomeState extends State<Home> {
               color: _indigo),
           child: Icon(Icons.add, size: 25.0, color: Colors.white),
         ),
-        backgroundColor: _indigo_shadow,
+        backgroundColor: _indigoShadow,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
@@ -70,27 +111,29 @@ class HomeState extends State<Home> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     IconButton(
-                      icon: Icon(Icons.home, size: 25.0, color: _normal_icon_1),
+                      icon: Icon(Icons.home, size: 25.0, color: _normalIcon1),
                       onPressed: () {
                         setState(() {
-                          if (_normal_icon_1 == _grey) {
-                            _normal_icon_2 = _grey;
-                            _normal_icon_3 = _grey;
-                            _normal_icon_4 = _grey;
-                            _normal_icon_1 = _indigo2;
+                          if (_normalIcon1 == _grey) {
+                            _normalIcon2 = _grey;
+                            _normalIcon3 = _grey;
+                            _normalIcon4 = _grey;
+                            _normalIcon1 = _indigo2;
+                            currentPage = pages[0];
                           }
                         });
                       },
                     ),
                     IconButton(
-                      icon: Icon(Icons.apps, size: 25.0, color: _normal_icon_2),
+                      icon: Icon(Icons.stars, size: 25.0, color: _normalIcon2),
                       onPressed: () {
                         setState(() {
-                          if (_normal_icon_2 == _grey) {
-                            _normal_icon_1 = _grey;
-                            _normal_icon_3 = _grey;
-                            _normal_icon_4 = _grey;
-                            _normal_icon_2 = _indigo2;
+                          if (_normalIcon2 == _grey) {
+                            _normalIcon1 = _grey;
+                            _normalIcon3 = _grey;
+                            _normalIcon4 = _grey;
+                            _normalIcon2 = _indigo2;
+                            currentPage = pages[1];
                           }
                         });
                       },
@@ -102,29 +145,31 @@ class HomeState extends State<Home> {
                     SizedBox.shrink(),
                     SizedBox.shrink(),
                     IconButton(
-                      icon: Icon(Icons.satellite,
-                          size: 25.0, color: _normal_icon_3),
+                      icon:
+                          Icon(Icons.security, size: 25.0, color: _normalIcon3),
                       onPressed: () {
                         setState(() {
-                          if (_normal_icon_3 == _grey) {
-                            _normal_icon_1 = _grey;
-                            _normal_icon_2 = _grey;
-                            _normal_icon_4 = _grey;
-                            _normal_icon_3 = _indigo2;
+                          if (_normalIcon3 == _grey) {
+                            _normalIcon1 = _grey;
+                            _normalIcon2 = _grey;
+                            _normalIcon4 = _grey;
+                            _normalIcon3 = _indigo2;
+                            currentPage = pages[2];
                           }
                         });
                       },
                     ),
                     IconButton(
-                      icon: Icon(Icons.settings,
-                          size: 25.0, color: _normal_icon_4),
+                      icon:
+                          Icon(Icons.settings, size: 25.0, color: _normalIcon4),
                       onPressed: () {
                         setState(() {
-                          if (_normal_icon_4 == _grey) {
-                            _normal_icon_1 = _grey;
-                            _normal_icon_2 = _grey;
-                            _normal_icon_3 = _grey;
-                            _normal_icon_4 = _indigo2;
+                          if (_normalIcon4 == _grey) {
+                            _normalIcon1 = _grey;
+                            _normalIcon2 = _grey;
+                            _normalIcon3 = _grey;
+                            _normalIcon4 = _indigo2;
+                            currentPage = pages[3];
                           }
                         });
                       },
@@ -132,6 +177,114 @@ class HomeState extends State<Home> {
                   ],
                 ),
               ))),
+    );
+  }
+}
+
+class PageOne extends StatefulWidget {
+  PageOne({Key key}) : super(key: key);
+
+  @override
+  PageOneState createState() => PageOneState();
+}
+
+class PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+        padding: EdgeInsets.all(8.0),
+        child: Material(
+          elevation: 4.0,
+          borderRadius: BorderRadius.circular(5.0),
+          color: index % 2 == 0 ? Colors.green : Colors.yellow,
+          child: Center(
+            child: Text(index.toString()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatefulWidget {
+  PageTwo({Key key}) : super(key: key);
+
+  @override
+  PageTwoState createState() => PageTwoState();
+}
+
+class PageTwoState extends State<PageTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+        padding: EdgeInsets.all(8.0),
+        child: Material(
+          elevation: 4.0,
+          borderRadius: BorderRadius.circular(5.0),
+          color: index % 2 == 0 ? Colors.cyan : Colors.deepOrange,
+          child: Center(
+            child: Text(index.toString()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageThree extends StatefulWidget {
+  PageThree({Key key}) : super(key: key);
+
+  @override
+  PageThreeState createState() => PageThreeState();
+}
+
+class PageThreeState extends State<PageThree> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+        padding: EdgeInsets.all(8.0),
+        child: Material(
+          elevation: 4.0,
+          borderRadius: BorderRadius.circular(5.0),
+          color: index % 2 == 0 ? Colors.pink : Colors.white,
+          child: Center(
+            child: Text(index.toString()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageFour extends StatefulWidget {
+  PageFour({Key key}) : super(key: key);
+
+  @override
+  PageFourState createState() => PageFourState();
+}
+
+class PageFourState extends State<PageFour> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+        padding: EdgeInsets.all(8.0),
+        child: Material(
+          elevation: 4.0,
+          borderRadius: BorderRadius.circular(5.0),
+          color: index % 2 == 0 ? Colors.yellow : Colors.purple,
+          child: Center(
+            child: Text(index.toString()),
+          ),
+        ),
+      ),
     );
   }
 }
