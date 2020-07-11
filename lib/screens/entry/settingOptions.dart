d747232a08ae3e469e9e7ac614891e15d8298e0d
peetacho <peterchow2001@gmail.com>
2020-07-11 17:44:28 +0800

    544pm july11 made portraite, added report bug

--- lib/screens/entry/settingOptions.dart
+++ lib/screens/entry/settingOptions.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 Color _background = Color.fromRGBO(240, 243, 250, 1);
+Color _searchBarColor = Color.fromRGBO(229, 233, 244, 1);
 
 class SettingOption extends StatefulWidget {
   final String option;
@@ -30,7 +32,7 @@ class SettingOptionState extends State<SettingOption> {
         return _settingsPass();
         break;
       case 'Report a Bug':
-        return Text(option);
+        return _settingsBug();
         break;
       case 'Credits':
         return _settingsCredits();
@@ -104,39 +106,152 @@ class SettingOptionState extends State<SettingOption> {
 
   _settingsPass() {}
 
-  // void displayDialog() {
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) => new CupertinoAlertDialog(
-  //       title: Text('Delete all entries?'),
-  //       content: Text('This action can not be reverted.'),
-  //       actions: <Widget>[
-  //         FlatButton(
-  //             onPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //             child: Text('Cancel', style: TextStyle(color: Colors.red))),
-  //         FlatButton(
-  //             onPressed: () {
-  //               deleteAll();
-  //               Navigator.of(context).pop();
-  //             },
-  //             child: Text('Delete', style: TextStyle(color: Colors.blue))),
-  //       ],
-  //     ),
-  //   );
-  // }
+  final formKey = GlobalKey<FormState>();
+  String _userSubject, _userBody;
 
-  // deleteAll() {
-  //   if (items != null) {
-  //     debugPrint('items BEFORE delete: ' + items.toList().toString());
-  //     items.clear();
-  //     itemsToJSON = jsonEncode(items);
-  //     debugPrint('items AFTER delete: ' + itemsToJSON);
-  //     debugPrint('all entries deleted');
-  //     JSONStorage.writeJSONStorage(itemsToJSON);
-  //   }
-  // }
+  _settingsBug() {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    _emailSubject(),
+                    _emailBody(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                        child: SizedBox(
+                      height: 55,
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      child: FlatButton(
+                          color: Colors.green[400], //entryColor2[0],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Send Bug Report',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          onPressed: () {
+                            send();
+                            //send();
+                          }),
+                    )),
+                  ],
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _emailSubject() {
+    return Container(
+      margin: EdgeInsets.only(top: 8.0, right: 8.0),
+      width: MediaQuery.of(context).size.width * 0.85,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 5.0),
+            child: Text(
+              'Subject',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+            ),
+          ),
+          TextFormField(
+            validator: (input) =>
+                input.length < 1 ? 'Please input an account' : null,
+            onSaved: (input) => _userSubject = input,
+            readOnly: false,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: _searchBarColor,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: _searchBarColor),
+                    borderRadius: BorderRadius.circular(8.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: _searchBarColor),
+                    borderRadius: BorderRadius.circular(8.0))),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _emailBody() {
+    return Container(
+      margin: EdgeInsets.only(top: 8.0, right: 8.0),
+      width: MediaQuery.of(context).size.width * 0.85,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 5.0),
+            child: Text(
+              'Message',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+            ),
+          ),
+          TextFormField(
+            validator: (input) =>
+                input.length < 0 ? 'Please input something' : null,
+            onSaved: (input) => _userBody = input,
+            readOnly: false,
+            maxLines: 5,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: _searchBarColor,
+                focusColor: _searchBarColor,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: _searchBarColor),
+                    borderRadius: BorderRadius.circular(8.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: _searchBarColor),
+                    borderRadius: BorderRadius.circular(8.0))),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> send() async {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      final Email email = Email(
+        body: _userBody,
+        subject: '[PV BUG REPORT:] ' + _userSubject,
+        recipients: ['peterworks2001@gmail.com'],
+        // attachmentPath: attachment,
+      );
+
+      String platformResponse;
+
+      try {
+        await FlutterEmailSender.send(email);
+        platformResponse = 'success';
+      } catch (error) {
+        platformResponse = error.toString();
+      }
+
+      if (!mounted) return;
+
+      debugPrint(platformResponse);
+      print(email.body + email.subject + email.recipients[0]);
+
+      formKey.currentState.reset();
+    }
+  }
 
   _settingsCredits() {
     return Container(
